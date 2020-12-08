8961f8de03f5f359cbd72bd371cf97c904ee7f0f
Killian <clota974@gmail.com>
2020-12-08 19:47:30 +0400

    dqsds

--- demo.pas
+++ demo.pas
@@ -588,7 +588,7 @@ begin
     textPos[i].w := 400;
     textPos[i].h := 100;
     textPos[i].x := 50 + 200 - floor(texte[i]^.w / 2);
-    textPos[i].y := (i+1)*110 + 50 + floor(texte[i]^.h / 2);
+    textPos[i].y := (i+1)*110 + 50 - floor(texte[i]^.h / 2);
 
     imageButton := IMG_Load('./res/button.png');
     if choice = i then imageButton := IMG_Load('./res/selected.png');
